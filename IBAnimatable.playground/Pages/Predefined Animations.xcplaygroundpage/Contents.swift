3cbe13cf85b79963dfd78606a5a30b6777360379
Tom Baranes <tom.baranes@gmail.com>
2016-02-10 08:28:38 +0100

    Refactoring moveTo to use CGPoint instead on two floats

--- IBAnimatable.playground/Pages/Predefined Animations.xcplaygroundpage/Contents.swift
+++ IBAnimatable.playground/Pages/Predefined Animations.xcplaygroundpage/Contents.swift
@@ -25,8 +25,8 @@ view.fillColor = UIColor(red: 0xba/0xff, green: 0x77/0xff, blue: 1, alpha: 1)
 view.maskType = "Circle"
 
 // Config moveX / MoveY / MoveXY animations
-view.xDest = 5
-view.yDest = 5
+view.originDest = CGPoint(x: 5, y: 5)
+
 //: Animations, all supported predefined animations can be found in `enum AnimationType`  
 
 // Uncomment one line to play the animation
